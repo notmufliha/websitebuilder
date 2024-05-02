0f31a6cfdc33c7284b49146c11f56f88f6a45d76
dariuschew <dariuschew1050@gmail.com>
2024-05-02 09:41:22 +0800

    nun

--- frontend/src/Home.js
+++ frontend/src/Home.js
@@ -21,7 +21,7 @@ const Home = () => {
 
   const { pageStore } = useSelector((state) => state);
   const { pages } = pageStore;
-  
+
   const handleSubmit = async () => {
     if (!name) {
       setIsValid(false);
@@ -67,199 +67,20 @@ const Home = () => {
     }
   };
 
-  // const handleExport = async () => {
-  //   const pageDataPromises = selectedPages.map(async (pageId) => {
-  //     try {
-  //       const response = await fetch(`http://localhost:8080/api/pages/${pageId}`);
-  //       const data = await response.json();
-  
-  //       // Clean the HTML string
-  //       const cleanedHTML = data.content['mycustom-html'];
-  //       const cleanedCSS = data.content['mycustom-css'];
-  //       const cleanedComponents = JSON.parse(data.content['mycustom-components']);
-  //       const cleanedAssets = JSON.parse(data.content['mycustom-assets']);
-  
-  //       return { html: cleanedHTML, css: cleanedCSS, components: cleanedComponents, assets: cleanedAssets, pageId, name: data.name };
-  
-  //     } catch (error) {
-  //       console.error('Error fetching page data:', error);
-  //       return null;
-  //     }
-  //   });
-  
-  //   // Wait for all page data promises to resolve
-  //   const pageData = await Promise.all(pageDataPromises);
-  
-  //   // Create a new ZIP archive 
-  //   const zip = new JSZip();
-  
-  //   // Create a CSS folder
-  //   const cssFolder = zip.folder('css');
-  
-  //   // Add the HTML and CSS for each page to the ZIP archive 
-  //   pageData.forEach(({ html, css, assets, pageId, name }) => {
-  //     if (html) {
-  //       // Construct HTML content with proper structure
-  //       const htmlContent = `
-  //         <!DOCTYPE html>
-  //         <html lang="en">
-  //         <head>
-  //           <meta charset="UTF-8">
-  //           <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  //           <title>${name}</title>
-  //           <link rel="stylesheet" href="css/style_${name.replace(/\s/g, '_').toLowerCase()}.css">
-  //         </head>
-  //         <body>
-  //           ${html}
-  //         </body>
-  //         </html>
-  //       `;
-  
-  //       // Add HTML file to the ZIP archive
-  //       zip.file(`${name}.html`, htmlContent);
-  
-  //       // Add CSS file to the CSS folder with naming convention style_pagename.css
-  //       cssFolder.file(`style_${name.replace(/\s/g, '_').toLowerCase()}.css`, css);
-  
-  //       // Add assets to the ZIP archive
-  //       assets.forEach((asset, index) => {
-  //         const filename = `images/${pageId}_image_${index + 1}.jpg`;
-  //         // Fetch assets and add them to the ZIP
-  //         // Adjust the path as per your server setup
-  //         fetch(asset.src, { mode: 'no-cors' })
-  //           .then(response => response.blob())
-  //           .then(blob => {
-  //             zip.file(filename, blob);
-  //           })
-  //           .catch(error => console.error('Error fetching asset:', error));
-  //       });
-  //     }
-  //   });
-  
-  //   // Generate the ZIP archive 
-  //   zip.generateAsync({ type: 'blob' }).then((content) => {
-  //     saveAs(content, 'pages.zip');
-  //   });
-  
-  //   setSelectedPages([]);
-  // };
-  
-  // return (
-  //   <div className="container py-5">
-  //     <div className="row justify-content-center">
-  //       <div className="col-md-8">
-  //         <form id="create-page">
-  //           <div className="card">
-  //             <div className="card-header">
-  //               <h5>Create a New Page</h5>
-  //             </div>
-  //             <div className="card-body">
-  //               <div className="mb-3">
-  //                 <label htmlFor="name" className="form-label">Page Name</label>
-  //                 <input
-  //                   type="text"
-  //                   className={`form-control ${isValid ? "" : "is-invalid"}`}
-  //                   id="name"
-  //                   placeholder="Enter page name"
-  //                   value={name}
-  //                   onChange={(e) => setName(e.target.value)}
-  //                 />
-  //                 {!isValid && (
-  //                   <div className="invalid-feedback">
-  //                     Please provide a valid name.
-  //                   </div>
-  //                 )}
-  //               </div>
-  //             </div>
-  //             <div className="card-footer text-end">
-  //               <button
-  //                 type="button"
-  //                 className="btn btn-secondary me-2"
-  //                 onClick={() => setName("")}
-  //               >
-  //                 Clear
-  //               </button>
-  //               <button
-  //                 type="button"
-  //                 className="btn btn-primary"
-  //                 onClick={handleSubmit}
-  //               >
-  //                 Save
-  //               </button>
-  //             </div>
-  //           </div>
-  //         </form>
-  //       </div>
-  //       <div className="col-md-12 mt-4">
-  //         <button
-  //           type="button"
-  //           className="btn btn-primary mb-3"
-  //           onClick={handleExport}
-  //           disabled={selectedPages.length === 0} // Disable the button if no pages are selected
-  //         >
-  //           Export
-  //         </button>
-  //         <div className="table-responsive">
-  //           <table className="table table-striped table-hover">
-  //             <thead className="table-dark">
-  //               <tr>
-  //                 <th>ID</th>
-  //                 <th>Name</th>
-  //                 <th>Slug</th>
-  //                 <th>Action</th>
-  //               </tr>
-  //             </thead>
-  //             <tbody>
-  //               {pages && pages.length > 0 ? pages.map((page) => (
-  //                 <tr key={page._id}>
-  //                   <td>
-  //                     <input
-  //                       type="checkbox"
-  //                       value={page._id}
-  //                       checked={selectedPages.includes(page._id)}
-  //                       onChange={() => handlePageCheckboxChange(page._id)}
-  //                     />
-  //                   </td>
-  //                   <td>{page._id}</td>
-  //                   <td>{page.name}</td>
-  //                   <td>{page.slug}</td>
-  //                   <td>
-  //                     <Link to={`/editor/${page._id}`} className="btn btn-info btn-sm">Edit</Link>
-  //                     <button
-  //                       type="button"
-  //                       className="btn btn-primary"
-  //                       onClick={() => confirmDelete(page._id)} 
-  //                     >
-  //                       Delete
-  //                     </button>
-  //                   </td>
-  //                 </tr>
-  //               )) : (
-  //                 <tr><td colSpan="4" className="text-center">No pages found</td></tr>
-  //               )}
-  //             </tbody>
-  //           </table>
-  //         </div>
-  //       </div>
-  //     </div>
-  //     <ToastContainer />
-  //   </div>
-  // );
-
   const handleExport = async () => {
     const pageDataPromises = selectedPages.map(async (pageId) => {
       try {
         const response = await fetch(`http://localhost:8080/api/pages/${pageId}`);
         if (!response.ok) throw new Error('Network response was not ok.');
         const data = await response.json();
-  
+
         // Check if data or content is null or empty
         if (!data || !data.content || !data.content['mycustom-html']) {
           // Notify user if the page data is null or empty
           toast.error(`Page ${data.name} has no content or does not exist.`);
           return null;
         }
-  
+
         // Clean the HTML string
         const cleanedHTML = data.content['mycustom-html'];
         const cleanedCSS = data.content['mycustom-css'];
@@ -278,22 +99,22 @@ const Home = () => {
 
     // Wait for all page data promises to resolve
     const pageData = await Promise.all(pageDataPromises);
-  
+
     // Filter out null entries before further processing
     const validPageData = pageData.filter(page => page !== null);
-  
+
     // If no valid page data, return without exporting
     if (validPageData.length === 0) {
       toast.error(`Page chosen has no content or does not exist.`);
       return;
     }
-  
+
     // Create a new ZIP archive 
     const zip = new JSZip();
 
     // Create a CSS folder
     const cssFolder = zip.folder('css');
-  
+
     // Add the HTML and CSS for each page to the ZIP archive 
     validPageData.forEach(({ html, css, assets, pageId, name }) => {
       if (html) {
@@ -333,38 +154,38 @@ const Home = () => {
         });
       }
     });
-  
+
     // Generate the ZIP archive 
     zip.generateAsync({ type: 'blob' }).then(async (content) => {
       toast.success(`Page successfully exported.`);
       saveAs(content, 'pages.zip');
     });
-  
+
     setSelectedPages([]);
   };
-  
+
 
   const handleExport2 = async () => {
     const pageDataPromises = selectedPages.map(async (pageId) => {
       try {
         const response = await fetch(`http://localhost:8080/api/pages/${pageId}`);
         const data = await response.json();
-  
+
         // Check if data or content is null or empty
         if (!data || !data.content || !data.content['mycustom-html']) {
           // Notify user if the page data is null or empty
           toast.error(`Page ${data.name} has no content or does not exist.`);
           return null;
         }
-  
+
         // Clean the HTML string
         const cleanedHTML = data.content['mycustom-html'];
         const cleanedCSS = data.content['mycustom-css'];
         const cleanedComponents = JSON.parse(data.content['mycustom-components']);
         const cleanedAssets = JSON.parse(data.content['mycustom-assets']);
-  
+
         return { html: cleanedHTML, css: cleanedCSS, components: cleanedComponents, assets: cleanedAssets, pageId, name: data.name };
-  
+
       } catch (error) {
         console.error('Error fetching page data:', error);
         // Notify user if there's an error fetching page data
@@ -372,21 +193,21 @@ const Home = () => {
         return null;
       }
     });
-  
+
     // Wait for all page data promises to resolve
     const pageData = await Promise.all(pageDataPromises);
-  
+
     // Filter out null entries before further processing
     const validPageData = pageData.filter(page => page !== null);
-  
+
     // If no valid page data, return without exporting
     if (validPageData.length === 0) {
       toast.error(`Page chosen has no content or does not exist.`);
       return;
     }
-  
+
     const filesArray = [];
-  
+
     // Add HTML and CSS files to the filesArray
     validPageData.forEach(({ html, css, name }) => {
       if (html && css) {
@@ -409,7 +230,7 @@ const Home = () => {
         filesArray.push({ name: `style_${name.replace(/\s/g, '_').toLowerCase()}.css`, content: css });
       }
     });
-  
+
     try {
       // Send the HTML and CSS files array to the server using fetch
       const response = await fetch('http://localhost:8080/api/pages/test', {
@@ -419,7 +240,7 @@ const Home = () => {
         },
         body: JSON.stringify({ files: filesArray }),
       });
-  
+
       if (response.ok) {
         toast.success(`Page successfully exported.`);
       } else {
@@ -429,12 +250,12 @@ const Home = () => {
       console.error('Error exporting page:', error);
       toast.error(`Failed to export page. ${error.message}`);
     }
-  
-    setSelectedPages([]);
-};
 
-  
-  
+    setSelectedPages([]);
+  };
+
+
+
   return (
     <div style={{ paddingTop: '40px', fontFamily: 'Arial, sans-serif' }}>
       <div style={{ maxWidth: '800px', margin: 'auto' }}>
@@ -481,9 +302,9 @@ const Home = () => {
             </div>
           </div>
         </form>
-       
-       
-       <div style={{ marginTop: '20px' }}>
+
+
+        <div style={{ marginTop: '20px' }}>
           <button
             type="button"
             style={{ padding: '10px 20px', background: '#007bff', color: '#fff', border: 'none', borderRadius: '4px', marginBottom: '10px' }}
@@ -523,7 +344,7 @@ const Home = () => {
                     </td>
                     <td style={{ padding: '10px', border: '1px solid #ddd' }}>{page._id}</td>
                     <td style={{ padding: '10px', border: '1px solid #ddd' }}>{page.name}</td>
-                                  <td style={{ padding: '10px', border: '1px solid #ddd' }}>
+                    <td style={{ padding: '10px', border: '1px solid #ddd' }}>
                       <Link to={`/editor/${page._id}`} style={{ padding: '5px 10px', background: '#17a2b8', color: '#fff', borderRadius: '4px', textDecoration: 'none' }}>Edit</Link>
                       <button
                         type="button"
@@ -547,79 +368,3 @@ const Home = () => {
   );
 };
 export default Home;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- // Fetch the HTML and CSS for each selected page from the backend
-  // const pageDataPromises = selectedPages.map(pageId =>
-  //     fetch(`http://localhost:8080/api/pages/${pageId}`)
-  //     .then(response => {
-  //       console.log(response.text())
-  //         if (!response.ok) {
-  //             throw new Error('Network response was not ok');
-  //         }
-  //         return response.text(); // Get the raw text content
-  //     })
-  //     .then(data => ({
-  //         html: data, // Assign HTML directly
-  //         pageId
-  //     }))
-  //     .catch(error => console.error('Error fetching page data:', error))
-  // );
-
-  // var parser = new DOMParser();
-
-  // Parse the text
-  // var doc = parser.parseFromString(html, "text/html");
-
-  // // Wait for all page data to be fetched 
-  // const pageData = await Promise.all(pageDataPromises);
-
-  // Create a new ZIP archive 
-  // const zip = new JSZip();
-
-  // Add the HTML for each page to the ZIP archive 
-  // pageData.forEach(({ html, pageId }) => {
-  //     const htmlContent = `
-  //         <html lang="en">
-  //         <head>
-  //             <meta charset="utf-8">
-  //             <link rel="stylesheet" href="./css/style.css">
-  //         </head>
-  //         <body>
-  //             ${html}
-  //         </body>
-  //         </html>
-  //     `;
-  //     zip.file(`page_${pageId}/index.html`, htmlContent);
-  // });
-
-  // // Generate the ZIP archive 
-  // zip.generateAsync({ type: 'blob' }).then((content) => {
-  //     saveAs(content, 'pages.zip');
-  // });
-
-  // Clear selected pages after export 
