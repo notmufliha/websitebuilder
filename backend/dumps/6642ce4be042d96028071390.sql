c67816e096d29eed8c48977eee1e77425c3adfca
dariuschew <dariuschew1050@gmail.com>
2024-05-16 14:18:38 +0800

    AHAHHAHAHAHA

--- backend/dumps/6642ce4be042d96028071390.sql
+++ backend/dumps/6642ce4be042d96028071390.sql
@@ -1,1 +1,21 @@
-CREATE DATABASE IF NOT EXISTS default_schema; USE default_schema;      CREATE TABLE IF NOT EXISTS components (       component_id INT AUTO_INCREMENT PRIMARY KEY,       page_id VARCHAR(255)     );      CREATE TABLE IF NOT EXISTS component_attributes (       component_id INT,       attribute_key VARCHAR(255),       attribute_value TEXT,       PRIMARY KEY (component_id, attribute_key),       FOREIGN KEY (component_id) REFERENCES components(component_id)     );    INSERT INTO components (component_id, page_id) VALUES (93, '6642ce4be042d96028071390');
+CREATE DATABASE IF NOT EXISTS default_schema;
+
+USE default_schema;
+
+CREATE TABLE IF NOT EXISTS components (
+    component_id INT AUTO_INCREMENT PRIMARY KEY,
+    page_id VARCHAR(255)
+);
+
+CREATE TABLE IF NOT EXISTS component_attributes (
+    component_id INT,
+    attribute_key VARCHAR(255),
+    attribute_value TEXT,
+    PRIMARY KEY (component_id, attribute_key),
+    FOREIGN KEY (component_id) REFERENCES components(component_id)
+);
+
+INSERT INTO
+    components (component_id, page_id)
+VALUES
+    (93, '6642ce4be042d96028071390');
